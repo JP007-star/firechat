a335a30cd6fa4df719efaa761dffaa21467428dd
unknown <justice_of_peace@yahoo.com>
2022-05-20 01:40:31 +0530

    recent changes

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
 import SignOut from './SignOut'
+import { db } from '../firebase'
 
 function Chat() {
+  const [messages,setMessages]=useState([])
+  useEffect(()=>{
+      db.collection('messages').orderBy('createdBy').limit(50).onSnapshot(snapshot=>{
+            setMessages(snapshot.docs.map(doc=>doc.data))
+      })
+  },[])
+  console.log(messages.data);
   return (
 
     <div>
           <SignOut/>
-          <p>Chat</p>
+          {messages.map(({id,text,photoURL})=>{
+               <div key={id}>
+                  <img src={photoURL}  />
+                  <p>text</p>
+               </div>
+          })}
     </div>
   )
 }
